89f6d7d4da86260b378317be56ebb8fabc17c025
Andres Jaramillo <somoselcosmo@gmail.com>
2024-06-16 17:44:33 -0500

    Final

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+ {
+  htmlWhitespaceSensitivity: 'ignore',
+  endOfLine: 'auto',
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  htmlVoidTags: false
+};
